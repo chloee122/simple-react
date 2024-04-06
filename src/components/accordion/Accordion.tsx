96b288c574ea211ba52c3eea9447de24ca68a508
Chloe <nguyeney02@gmail.com>
2024-04-06 17:35:12 +0300

    accordion component

---
+++ src/components/accordion/Accordion.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import data from "./data";
+
+export default function Accordion() {
+  const [selected, setSelected] = useState<number>(-1);
+  const [enableMultiSelection, setEnableMultiSelection] =
+    useState<boolean>(false);
+  const [multiple, setMultiple] = useState<number[]>([]);
+
+  const handleSingleSelect = (nextId: number) => {
+    setSelected((currentId) => {
+      if (currentId === nextId) return -1;
+      return nextId;
+    });
+  };
+
+  const handleMultiSelect = (currentId: number) => {
+    const cpyMultiple: number[] = [...multiple];
+
+    const indexOfCurrent: number = cpyMultiple.indexOf(currentId);
+
+    if (indexOfCurrent === -1) {
+      cpyMultiple.push(currentId);
+    } else {
+      cpyMultiple.splice(indexOfCurrent, 1);
+    }
+
+    setMultiple(cpyMultiple);
+  };
+
+  const content = (
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-3">
+      <button
+        className="p-3 font-bold text-lg bg-indigo-300"
+        onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+      >
+        Enable Multiple Selections
+      </button>
+      <div className="w-3/6">
+        {data.length > 0 ? (
+          data.map((item) => {
+            const isExpanded = item.id === selected;
+            return (
+              <div className="bg-indigo-300 mb-2 px-2 py-3">
+                <div
+                  className="font-bold flex justify-between cursor-pointer"
+                  key={item.id}
+                  onClick={() =>
+                    enableMultiSelection
+                      ? handleMultiSelect(item.id)
+                      : handleSingleSelect(item.id)
+                  }
+                >
+                  <h2 className="">{item.label}</h2>
+                  <span>+</span>
+                </div>
+                {enableMultiSelection
+                  ? multiple.includes(item.id) && (
+                      <div className="h-auto">{item.content}</div>
+                    )
+                  : isExpanded && <div className="h-auto">{item.content}</div>}
+              </div>
+            );
+          })
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
+
+  return content;
+}
---
+++ src/components/accordion/data.ts
@@ -0,0 +1,34 @@
+export interface ItemType {
+  id: number;
+  label: string;
+  content: string;
+}
+
+const data: ItemType[] = [
+    {
+      id: 1,
+      label: "What are accordion components",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget augue quis sem dapibus vestibulum. Donec elit purus, commodo sit amet urna at, efficitur mattis turpis. Fusce at lorem sed mi vehicula molestie sed id augue. Ut lorem lacus, efficitur nec massa eu, euismod vulputate libero.",
+    },
+    {
+      id: 2,
+      label: "What are they used for",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget augue quis sem dapibus vestibulum. Donec elit purus, commodo sit amet urna at, efficitur mattis turpis. Fusce at lorem sed mi vehicula molestie sed id augue. Ut lorem lacus, efficitur nec massa eu, euismod vulputate libero.",
+    },
+    {
+      id: 3,
+      label: "Accordions are musical instruments",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget augue quis sem dapibus vestibulum. Donec elit purus, commodo sit amet urna at, efficitur mattis turpis. Fusce at lorem sed mi vehicula molestie sed id augue. Ut lorem lacus, efficitur nec massa eu, euismod vulputate libero.",
+    },
+    {
+      id: 4,
+      label: "Can I create accordion components with other framework",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget augue quis sem dapibus vestibulum. Donec elit purus, commodo sit amet urna at, efficitur mattis turpis. Fusce at lorem sed mi vehicula molestie sed id augue. Ut lorem lacus, efficitur nec massa eu, euismod vulputate libero.",
+    },
+  ]
+
+  export default data
